18541896bc0bcb355bdda143e5f24398f0921123
Leonardo Thibes <leonardothibes@gmail.com>
2018-03-11 20:20:55 -0300

    wip

--- configure.sh
+++ configure.sh
@@ -6,7 +6,8 @@ HERE=$(dirname $0)
 if [ ${SO} == "Darwin" ]; then
     SCRIPT="${HERE}/mac/configure.sh"
 else
-    SCRIPT="${HERE}/mac/configure.sh"
+    SCRIPT="${HERE}/linux/configure.sh"
 fi
 
+clear
 sh ${SCRIPT}
--- linux/configure.sh
+++ linux/configure.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
-echo "Linux"
+WHO=$(id -u)
+HERE=$(dirname $0)
+
+# Only root
+clear
+if [ ${WHO} -ne 0 ]; then
+	echo "É necessário ter permissão de root para rodar este script"
+	exit 1
+fi
+# Only root
+
+for DIR in $(${HERE}/${DIR})
+do
+    echo "$(${HERE}/${DIR})/configure.sh"
+done
