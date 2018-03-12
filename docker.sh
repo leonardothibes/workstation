a448bee4aea9a05e67a7d54205b2c02eaa87a7b4
Leonardo Thibes <leonardothibes@gmail.com>
2018-03-11 21:50:35 -0300

    wip

--- docker.sh
+++ docker.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-IMG=linuxmintd/mint18-amd64
-DIR=/workstation
+IMG=mint
+USER=leonardo
+DIR=/home/${USER}/workstation
 
 clear
-docker run --rm -it -v $(pwd):${DIR} -w ${DIR} ${IMG} bash
+docker run --rm -it -v $(pwd):${DIR} -w ${DIR} --user ${USER} ${IMG} bash
