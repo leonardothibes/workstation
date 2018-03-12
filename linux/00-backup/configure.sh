fe20d00136d304919ff8a78fae42f2852f302d5b
Leonardo Thibes <leonardothibes@gmail.com>
2018-03-11 23:04:10 -0300

    hotfix

--- linux/00-backup/configure.sh
+++ linux/00-backup/configure.sh
@@ -3,13 +3,13 @@
 echo " - Config files backup..."
 BKPDIR=/root/bkp-confs
 
-[ -d ${BKPDIR}/etc ]              || mkdir -p ${BKPDIR}
-[ -f ${BKPDIR}/etc/hosts ]        || cp /etc/hosts ${BKPDIR}
-[ -f ${BKPDIR}/etc/fstab ]        || cp /etc/fstab ${BKPDIR}
-[ -f ${BKPDIR}/etc/adduser.conf ] || cp /etc/adduser.conf ${BKPDIR}
-[ -f ${BKPDIR}/etc/profile ]      || cp /etc/profile ${BKPDIR}
-[ -f ${BKPDIR}/etc/bash.bashrc ]  || cp /etc/bash.bashrc ${BKPDIR}
+[ -d ${BKPDIR}/etc ]              || mkdir -p ${BKPDIR}/etc/etc
+[ -f ${BKPDIR}/etc/hosts ]        || cp /etc/hosts ${BKPDIR}/etc
+[ -f ${BKPDIR}/etc/fstab ]        || cp /etc/fstab ${BKPDIR}/etc
+[ -f ${BKPDIR}/etc/adduser.conf ] || cp /etc/adduser.conf ${BKPDIR}/etc
+[ -f ${BKPDIR}/etc/profile ]      || cp /etc/profile ${BKPDIR}/etc
+[ -f ${BKPDIR}/etc/bash.bashrc ]  || cp /etc/bash.bashrc ${BKPDIR}/etc
 
-[ -d ${BKPDIR}/etc/apt ]       || cp -Rf /etc/apt ${BKPDIR}
-[ -d ${BKPDIR}/etc/skel ]      || cp -Rf /etc/skel ${BKPDIR}
-[ -d ${BKPDIR}/etc/profile.d ] || cp -Rf /etc/profile.d ${BKPDIR}
+[ -d ${BKPDIR}/etc/apt ]       || cp -Rf /etc/apt ${BKPDIR}/etc
+[ -d ${BKPDIR}/etc/skel ]      || cp -Rf /etc/skel ${BKPDIR}/etc
+[ -d ${BKPDIR}/etc/profile.d ] || cp -Rf /etc/profile.d ${BKPDIR}/etc
