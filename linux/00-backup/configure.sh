37d952c9c1517a72b08eedd939d4c6ab9036c1dd
Leonardo Thibes <leonardothibes@gmail.com>
2018-03-11 22:17:20 -0300

    Implementando o backup

--- linux/00-backup/configure.sh
+++ linux/00-backup/configure.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
 echo "Fazendo backup das configurações..."
+BKPDIR=/root/bkp-confs
+
+[ -d ${BKPDIR}/etc ]              || mkdir -p ${BKPDIR}
+[ -f ${BKPDIR}/etc/hosts ]        || cp /etc/hosts ${BKPDIR}
+[ -f ${BKPDIR}/etc/fstab ]        || cp /etc/fstab ${BKPDIR}
+[ -f ${BKPDIR}/etc/adduser.conf ] || cp /etc/adduser.conf ${BKPDIR}
+[ -f ${BKPDIR}/etc/profile ]      || cp /etc/profile ${BKPDIR}
+[ -f ${BKPDIR}/etc/bash.bashrc ]  || cp /etc/bash.bashrc ${BKPDIR}
+
+[ -d ${BKPDIR}/etc/apt ]       || cp -Rf /etc/apt ${BKPDIR}
+[ -d ${BKPDIR}/etc/skel ]      || cp -Rf /etc/skel ${BKPDIR}
+[ -d ${BKPDIR}/etc/profile.d ] || cp -Rf /etc/profile.d ${BKPDIR}
