06d3aa714177121945e96b82a9d34756cd9dd0e2
Leonardo Thibes <leonardothibes@gmail.com>
2018-04-21 19:17:46 -0300

    wip

--- linux/02-skel/configure.sh
+++ linux/02-skel/configure.sh
@@ -2,3 +2,16 @@
 
 echo " - Home skeleton configuration..."
 CONFS=$(dirname $0)/confs
+
+[ -d /etc/skel/tmp ]  || mkdir /etc/skel/tmp
+[ -d /etc/skel/.bin ] || mkdir /etc/skel/.bin
+[ -d /etc/skel/.ssh ] || mkdir /etc/skel/.ssh
+
+cp -f ${CONFS}/.profile /etc/skel
+cp -f ${CONFS}/.bash_logout /etc/skel
+rm -f /etc/skel/.bashrc /root/.bashrc
+
+for DIR in `ls /home`
+do
+    echo "USER: ${DIR}"
+done;
--- linux/02-skel/confs/.profile
+++ linux/02-skel/confs/.profile
@@ -8,10 +8,9 @@
 # for ssh logins, install and configure the libpam-umask package.
 #umask 022
 
-# if running bash
 if [ -n "$BASH_VERSION" ]; then
-    # include .bashrc if it exists
     if [ -f "$HOME/.bashrc" ]; then
+    # include .bashrc if it exists
 	. "$HOME/.bashrc"
     fi
 fi
