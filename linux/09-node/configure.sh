3992c8a6340f7fca2bee48ad4c9df21b532f586d
Leonardo Thibes <leonardothibes@gmail.com>
2018-04-22 02:54:55 -0300

    nvm

--- linux/09-node/configure.sh
+++ linux/09-node/configure.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
 echo " - Node.js..."
+CONFS=$(dirname $0)/confs
 
+for USER in $(ls /home)
+do
+    su - ${USER} -c "wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.9/install.sh | bash > /dev/null 2>&1"
+done;
 
+cp -f ${CONFS}/nvm.sh /etc/profile.d
