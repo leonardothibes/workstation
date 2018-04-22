bb08cad78d38d91b6f75829b82e2881ebd92f47e
Leonardo Thibes <leonardothibes@gmail.com>
2018-04-22 03:42:39 -0300

    Dotnet

---
+++ linux/10-dotnet/configure.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo " - Dotnet Core..."
+
+# Repository
+curl -s https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-ubuntu-xenial-prod xenial main" > /etc/apt/sources.list.d/dotnetdev.list
+# Repository
+
+# Install
+apt-get update > /dev/null 2>&1
+apt-get install -y apt-transport-https dotnet-sdk-2.1.105 > /dev/null 2>&1
+# Install
