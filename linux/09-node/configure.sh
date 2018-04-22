2ce54d429ae63e7f99d0610598ce8d2fd8ec5c35
Leonardo Thibes <leonardothibes@gmail.com>
2018-04-22 03:17:42 -0300

    nodejs

--- linux/09-node/configure.sh
+++ linux/09-node/configure.sh
@@ -6,6 +6,8 @@ CONFS=$(dirname $0)/confs
 for USER in $(ls /home)
 do
     su - ${USER} -c "wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.9/install.sh | bash > /dev/null 2>&1"
+    su - ${USER} -c "nvm install 8.11.1 > /dev/null 2>&1"
+    su - ${USER} -c "npm install -g gulp http-server npm-check-updates > /dev/null 2>&1"
 done;
 
 cp -f ${CONFS}/nvm.sh /etc/profile.d
