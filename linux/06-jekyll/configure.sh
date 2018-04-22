d5cfd0045a90106663dc0aab85199e8c437bff32
Leonardo Thibes <leonardothibes@gmail.com>
2018-04-21 21:32:17 -0300

    lock no jekyll

--- linux/06-jekyll/configure.sh
+++ linux/06-jekyll/configure.sh
@@ -2,5 +2,10 @@
 
 echo " - Jekyll..."
 
+LOCK=/tmp/workstation.lock.update
+[ -f ${LOCK} ] && return
+
 apt-get install -y ruby ruby-dev > /dev/null
 gem install jekyll bundler sass  > /dev/null
+
+> ${LOCK}
