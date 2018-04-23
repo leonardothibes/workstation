ca0a25602710c303f879ba1cc7fc200616bf4dd1
Leonardo Thibes <leonardothibes@gmail.com>
2018-04-22 22:27:46 -0300

    Ajustes na isntalação do Docker

--- linux/12-docker/configure.sh
+++ linux/12-docker/configure.sh
@@ -3,11 +3,16 @@
 echo " - Docker..."
 CONFS=$(dirname $0)/confs
 
+# Docker
 echo "deb [arch=amd64] https://download.docker.com/linux/ubuntu xenial stable" > /etc/apt/sources.list.d/docker.list
 apt-get update > /dev/null 2>&1
 
-apt-get install -y docker-ce      \
-#                    docker-compose \
-> /dev/null 2>&1
-
+apt-get install -y docker-ce > /dev/null 2>&1
 cp -f ${CONFS}/docker.sh /etc/profile.d
+# Docker
+
+# Docker Compose
+VERSION=1.21.0
+sudo curl -L https://github.com/docker/compose/releases/download/${VERSION}/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+chmod 755 /usr/local/bin/docker-compose
+# Docker Compose
