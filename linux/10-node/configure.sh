fd4a885ecfefee5ec4ed37a9c389c2b118d51095
Leonardo Thibes <leonardothibes@gmail.com>
2018-04-30 23:56:03 -0300

    sintax minor hotfix

--- linux/10-node/configure.sh
+++ linux/10-node/configure.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
 echo " - Node.js..."
+CONFS=$(dirname $0)/confs
 
 VERSION=8.11.1
-CONFS=$(dirname $0)/confs
+PACKAGES="
+    npm-check-updates
+    http-server
+    gulp
+    uuid
+"
 
 LOCK=/tmp/workstation.lock.node
 [ -f ${LOCK} ] && return
@@ -12,7 +18,10 @@ for USER in $(ls /home)
 do
     su - ${USER} -c "wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.9/install.sh | bash > /dev/null 2>&1"
     su - ${USER} -c "nvm install ${VERSION} > /dev/null 2>&1"
-    su - ${USER} -c "npm install -g gulp http-server npm-check-updates uuid > /dev/null 2>&1"
+    for PACKAGE in ${PACKAGES}
+    do
+        su - ${USER} -c "npm install -g ${PACKAGE} > /dev/null 2>&1"
+    done;
 done;
 
 cp -f ${CONFS}/nvm.sh /etc/profile.d
