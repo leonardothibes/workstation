529965755df605025cfc4944bd20401c491fc98f
Leonardo Thibes <leonardothibes@gmail.com>
2018-11-03 14:24:54 -0300

    Tornando possível executar o Docker por todos os usuários sem necessidade de sudo

--- linux/13-docker/configure.sh
+++ linux/13-docker/configure.sh
@@ -9,13 +9,23 @@ LOCK=/tmp/workstation.lock.docker
 # Docker
 echo "deb [arch=amd64] https://download.docker.com/linux/ubuntu xenial stable" > /etc/apt/sources.list.d/docker.list
 apt-get update > /dev/null 2>&1
-
 apt-get install -y --allow-unauthenticated docker-ce > /dev/null 2>&1
-cp -f ${CONFS}/docker.sh /etc/profile.d
 # Docker
 
+# Docker lock file
+for USER in $(ls /home)
+do
+    usermod -aG docker ${USER}
+done
+# Docker lock file
+
+# Starting docker deamon
+systemctl enable docker > /dev/null 2>&1
+service docker start    > /dev/null 2>&1
+# Starting docker deamon
+
 # Docker Compose
-VERSION=1.21.0
+VERSION=1.21.0 # Atualizar versão para 1.22.0
 sudo curl -s -L https://github.com/docker/compose/releases/download/${VERSION}/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 chmod 755 /usr/local/bin/docker-compose
 # Docker Compose
--- linux/13-docker/confs/docker.sh
+++
@@ -1,4 +0,0 @@
-if [ $(id -u) -ne 0 ]; then
-    alias docker='sudo docker'
-    alias docker-compose='sudo docker-compose'
-fi
