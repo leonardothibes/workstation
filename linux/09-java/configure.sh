5674d8405c21142317bf856b207f5b3c4d7fc89f
Leonardo Thibes <leonardothibes@gmail.com>
2019-03-11 15:25:19 -0300

    Deixando de instalar o OpenJDK para usar o java oficial

--- linux/09-java/configure.sh
+++ linux/09-java/configure.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
 echo " - Java..."
+CONFS=$(dirname $0)/confs
 
-apt-get install -y openjdk-9-jre > /dev/null 2>&1
+VERSION=jre1.8.0_201
+INSTALL=/usr/local/java
+FILE=jre-8u201-linux-x64.tar.gz
+BIN=/usr/local/bin/java
+
+LOCK=/tmp/workstation.lock.java
+[ -f ${LOCK} ] && return
+
+[ -d ${INSTALL} ]            || mkdir ${INSTALL}
+[ -d ${INSTALL}/${VERSION} ] || tar -xzf ${CONFS}/${FILE} -C ${INSTALL}
+[ -L ${INSTALL}/current ]    || ln -sf ${INSTALL}/${VERSION} ${INSTALL}/current
+[ -L ${BIN} ]                || ln -sf ${INSTALL}/current/bin/java ${BIN}
+
+> ${LOCK}
