faed6ec952457f315ca0777a1060e7843ecb7b98
Leonardo Thibes <leonardothibes@gmail.com>
2019-03-12 15:17:19 -0300

    hotfix

--- linux/15-git/configure.sh
+++ linux/15-git/configure.sh
@@ -8,6 +8,6 @@ LOCK=/tmp/workstation.lock.git
 
 cp -f ${CONFS}/git-core-ppa-xenial.list /etc/apt/sources.list.d
 apt-get update > /dev/null 2>&1
-apt-get install -y --allow-unauthenticated git 2>&1
+apt-get install -y --allow-unauthenticated git > /dev/null 2>&1
 
 > ${LOCK}
