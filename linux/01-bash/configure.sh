c67bd145f4061f7878f4fdc16e0973f64c21afca
Leonardo Thibes <leonardothibes@gmail.com>
2019-03-12 14:58:08 -0300

    Variáveis de ambiente

--- linux/01-bash/configure.sh
+++ linux/01-bash/configure.sh
@@ -5,6 +5,4 @@ CONFS=$(dirname $0)/confs
 
 cp -f ${CONFS}/profile     /etc
 cp -f ${CONFS}/bash.bashrc /etc
-
-cp -f ${CONFS}/aliases.sh /etc/profile.d
-cp -f ${CONFS}/ps1.sh     /etc/profile.d
+cp -f ${CONFS}/*.sh        /etc/profile.d
