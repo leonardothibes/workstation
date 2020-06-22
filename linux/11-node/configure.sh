ec527325f990365941c226f5d1129ee12f213f8a
Leonardo Thibes <leonardothibes@gmail.com>
2020-06-21 21:18:16 -0300

    hotfix

--- linux/11-node/configure.sh
+++ linux/11-node/configure.sh
@@ -28,8 +28,15 @@ do
     su - ${USER} -c "nvm install ${VERSION} > /dev/null 2>&1"
     for PACKAGE in ${PACKAGES}
     do
-        su - ${USER} -c "npm install -g ${PACKAGE} > /dev/null 2>&1"
+        su - ${USER} -c "npm install --ignore-scripts -g ${PACKAGE} > /dev/null 2>&1"
     done;
+done;
+
+su - root -c "wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash > /dev/null 2>&1"
+su - root -c "nvm install ${VERSION} > /dev/null 2>&1"
+for PACKAGE in ${PACKAGES}
+do
+    su - root -c "npm install --ignore-scripts -g ${PACKAGE} > /dev/null 2>&1"
 done;
 
 cp -f ${CONFS}/nvm.sh /etc/profile.d
