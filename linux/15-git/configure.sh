4b1c682b79de79a8ce5ec20403ca04a2aa921d01
Leonardo Thibes <leonardothibes@gmail.com>
2020-06-25 09:01:37 -0300

    hotfix

--- linux/15-git/configure.sh
+++ linux/15-git/configure.sh
@@ -3,11 +3,4 @@
 echo " - GIT..."
 CONFS=$(dirname $0)/confs
 
-LOCK=/tmp/workstation.lock.git
-[ -f ${LOCK} ] && return
-
-cp -f ${CONFS}/git-core-ppa-xenial.list /etc/apt/sources.list.d
-apt-get update > /dev/null 2>&1
-apt-get install -y --allow-unauthenticated git > /dev/null 2>&1
-
-> ${LOCK}
+apt-get install -y git > /dev/null 2>&1
