d6f784d339aa48ea4183699e80bfe11b101a392d
Leonardo Thibes <leonardothibes@gmail.com>
2021-03-30 18:07:53 -0300

    Deixando o ZSH global

--- linux/17-zsh/configure.sh
+++ linux/17-zsh/configure.sh
@@ -3,4 +3,11 @@
 echo " - ZSH..."
 CONFS=$(dirname $0)/confs
 
-apt-get install -y zsh > /dev/null 2>&1
+apt-get install -y zsh fonts-powerline > /dev/null 2>&1
+
+rm -Rf /etc/skel/.oh-my-zsh
+git clone https://github.com/robbyrussell/oh-my-zsh.git /etc/skel/.oh-my-zsh > /dev/null 2>&1
+
+cp ${CONFS}/.zshrc /etc/skel/.zshrc
+mkdir -p /etc/skel/.oh-my-zsh/cache
+chmod 755 /etc/skel/.oh-my-zsh/oh-my-zsh.sh
