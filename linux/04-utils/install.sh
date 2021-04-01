f7c035865f5f0b19c9a4f94a79bb2751edad2399
Leonardo Thibes <leonardothibes@gmail.com>
2021-04-01 16:39:28 -0300

    atualizando cliente do mysql/mariadb

--- linux/04-utils/install.sh
+++ linux/04-utils/install.sh
@@ -3,15 +3,15 @@
 echo " - Generic utilities..."
 
 apt-get install -y apt-utils > /dev/null 2>&1
-apt-get install -y apt-transport-https   \
-                   wget                  \
-                   curl                  \
-                   nmap                  \
-                   nscd                  \
-                   tcpdump               \
-                   dialog                \
-                   xclip                 \
-                   unzip                 \
-                   build-essential       \
-                   mysql-client-core-5.7 \
+apt-get install -y apt-transport-https       \
+                   wget                      \
+                   curl                      \
+                   nmap                      \
+                   nscd                      \
+                   tcpdump                   \
+                   dialog                    \
+                   xclip                     \
+                   unzip                     \
+                   build-essential           \
+                   mariadb-client-core-10.3  \
 > /dev/null 2>&1
