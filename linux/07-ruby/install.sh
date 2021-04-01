9b68dd11f3ece29c756b62bda06221463c97c064
Leonardo Thibes <leonardothibes@gmail.com>
2021-04-01 15:21:48 -0300

    Consertando o ruby e tirando o jekyll pq dá muito problema

--- linux/07-ruby/install.sh
+++ linux/07-ruby/install.sh
@@ -2,10 +2,10 @@
 
 echo " - Ruby..."
 
-LOCK=/tmp/workstation.lock.jekyll
+LOCK=/tmp/workstation.lock.ruby
 [ -f ${LOCK} ] && return
 
 apt-get install -y ruby ruby-dev > /dev/null
-gem install jekyll bundler sass  > /dev/null
+gem install sass bundler         > /dev/null
 
 > ${LOCK}
