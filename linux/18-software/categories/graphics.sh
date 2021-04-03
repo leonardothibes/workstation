b68ee21fb3bdd9f3933fdb0c3d37f81418dcf5ff
Leonardo Thibes <leonardothibes@gmail.com>
2021-04-03 03:57:06 -0300

    Alguns softwares gráficos

--- linux/18-software/categories/graphics.sh
+++ linux/18-software/categories/graphics.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
 echo "   - Graphics..."
+
+# From repository
+apt-get install -y gimp     \
+                   inkscape \
+                   gpick    \
+> /dev/null 2>&1
+# From repository
