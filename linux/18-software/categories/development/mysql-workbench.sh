6e6e52a1f06e7fc3194094dd590174ff49f37ba6
Leonardo Thibes <leonardothibes@gmail.com>
2021-04-02 23:20:33 -0300

    Instalação do MySql Workbench (sim, tive que incluir o arquivo)

---
+++ linux/18-software/categories/development/mysql-workbench.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "       - Mysql Workbench..."
+
+apt-get install -y libzip5 > /dev/null 2>&1
+dpkg -i $(dirname $0)/files/mysql-workbench-community_8.0.23-1ubuntu20.04_amd64.deb > /dev/null 2>&1
