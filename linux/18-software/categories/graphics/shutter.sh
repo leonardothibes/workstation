bcf4a259dacac7b038606565501bcc2029a52326
Leonardo Thibes <leonardothibes@gmail.com>
2021-04-03 04:22:12 -0300

    wip

---
+++ linux/18-software/categories/graphics/shutter.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "       - Shutter..."
+
+# add-apt-repository ppa:linuxuprising/shutter > /dev/null 2>&1
+
+APT="/etc/apt/sources.list.d/linuxuprising-shutter-focal.list"
+echo "deb http://ppa.launchpad.net/linuxuprising/shutter/ubuntu focal main"     >  ${APT}
+echo "deb-src http://ppa.launchpad.net/linuxuprising/shutter/ubuntu focal main" >> ${APT}
+
+apt-get update          > /dev/null 2>&1
+apt-get install shutter > /dev/null 2>&1
