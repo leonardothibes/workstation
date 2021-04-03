1683d71778ce5b5424192226bd1b6a5fa73f28d8
Leonardo Thibes <leonardothibes@gmail.com>
2021-04-02 22:29:29 -0300

    Loop de scripts

--- linux/18-software/categories/development.sh
+++ linux/18-software/categories/development.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
+# From repository
 echo "   - Development..."
-OTHER=$(dirname $0)/development
 
 apt-get install -y sqlitebrowser \
                    gitg          \
                    meld          \
                    sublime-text  \
 > /dev/null 2>&1
+# From repository
 
+# Other sources
 echo "     - 3rd party..."
-sh ${OTHER}/vscode.sh
+
+DEVELOPMENT=$(dirname $0)/development
+for SOFTWARE in $(ls ${DEVELOPMENT})
+do
+    chmod 755 ${DEVELOPMENT}/${SOFTWARE}
+    sh ${DEVELOPMENT}/${SOFTWARE}
+done
+# Other sources
