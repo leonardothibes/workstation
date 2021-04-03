e3a136f5824f21b4fc0d4c14237611b51f2af66f
Leonardo Thibes <leonardothibes@gmail.com>
2021-04-02 22:32:00 -0300

    lock

--- linux/18-software/categories/development/azurestudio.sh
+++ linux/18-software/categories/development/azurestudio.sh
@@ -2,6 +2,9 @@
 
 echo "       - Azure Data Studio..."
 
+LOCK=/tmp/workstation.lock.azurestudio
+[ -f ${LOCK} ] && return
+
 URL="https://sqlopsbuilds.azureedge.net/stable/765c7064d46253d88393e0e1ff78874b6fe8fa64/azuredatastudio-linux-1.27.0.deb"
 TMP=/tmp
 
@@ -12,4 +15,6 @@ apt-get install -y libxss1      \
 
 curl -s ${URL} -o ${TMP}/azurestudio.deb
 dpkg -i ${TMP}/azurestudio.deb > /dev/null 2>&1
+
 rm -f ${TMP}/azurestudio.deb
+> ${LOCK}
