1fd1c8992e5293ed5f4fb4e6926408dc148de4d6
Leonardo Thibes <leonardothibes@gmail.com>
2021-08-18 08:18:56 -0300

    Implementando a sessão de multimedia

--- linux/18-software/categories/multimedia.sh
+++ linux/18-software/categories/multimedia.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
 echo "   - Multimedia..."
+
+# Clean
+apt-get remove -y rhythmbox
+
+# From repository
+apt-get install -y audacity       \
+                   handbrake      \
+                   kazam          \
+                   kdenlive       \
+                   spotify-client \
+> /dev/null 2>&1
+# From repository
