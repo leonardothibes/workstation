a4485ad2cb7aad01fc3545c24b5e9cb475cd47c5
Leonardo Thibes <leonardothibes@gmail.com>
2021-08-18 06:57:35 -0300

    Instalando oh-my-zsh para todos os usuários

--- linux/17-zsh/install.sh
+++ linux/17-zsh/install.sh
@@ -11,3 +11,13 @@ git clone https://github.com/robbyrussell/oh-my-zsh.git /etc/skel/.oh-my-zsh > /
 cp ${CONFS}/.zshrc /etc/skel/.zshrc
 mkdir -p /etc/skel/.oh-my-zsh/cache
 chmod 755 /etc/skel/.oh-my-zsh/oh-my-zsh.sh
+
+for USER in $(ls /home)
+do
+    HOME_FOLDER=/home/${USER}
+
+    cp -Rf /etc/skel/.zshrc     ${HOME_FOLDER}
+    cp -Rf /etc/skel/.oh-my-zsh ${HOME_FOLDER}
+
+    chown -R ${USER}: ${HOME_FOLDER}/.oh-my-zsh
+done;
