5fdd71bdc76b01c9993fc97bb068ceb627ccdfc3
Leonardo Thibes <leonardothibes@gmail.com>
2023-03-09 17:02:46 -0300

    wip

--- install.sh
+++ install.sh
@@ -25,6 +25,15 @@ else
 fi
 # SO
 
+sh ${HERE}/${SO}/00-backup/install.sh
+sh ${HERE}/${SO}/01-bash/install.sh
+sh ${HERE}/${SO}/02-skel/install.sh
+sh ${HERE}/${SO}/03-updates/install.sh
+sh ${HERE}/${SO}/04-utils/install.sh
+sh ${HERE}/${SO}/05-sysctl/install.sh
+# sh ${HERE}/${SO}/06-vim/install.sh
+exit 0
+
 # The magic happens here
 for DIR in $(ls ${HERE}/${SO})
 do
