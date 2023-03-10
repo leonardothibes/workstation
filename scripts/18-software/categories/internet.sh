e508e22717914ce3c2c3ca22fc8cea3a35022624
Leonardo Thibes <leonardothibes@gmail.com>
2023-03-10 14:52:27 -0300

    Softwares

--- scripts/18-software/categories/internet.sh
+++ scripts/18-software/categories/internet.sh
@@ -2,9 +2,28 @@
 
 echo "   - Internet..."
 
+LOCK=/tmp/workstation.lock.internet
+# [ -f ${LOCK} ] && return
+
 # From repository
-apt-get install -y skypeforlinux     \
-                   whatsapp-desktop  \
-                   slack             \
+apt-get install -y skypeforlinux      \
+                   filezilla          \
+                   slack              \
+                   remmina            \
+                   remmina-plugin-vnc \
+                   remmina-plugin-rdp \
 > /dev/null 2>&1
 # From repository
+
+# Other sources
+echo "     - 3rd party..."
+
+INTERNET=$(dirname $0)/internet
+for SOFTWARE in $(ls ${INTERNET})
+do
+    chmod 755 ${INTERNET}/${SOFTWARE}
+    sh ${INTERNET}/${SOFTWARE}
+done
+# Other sources
+
+> ${LOCK}
