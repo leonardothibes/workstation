ad8d9f7147804343833f165e21da75f93a4a226c
Leonardo Thibes <leonardothibes@gmail.com>
2023-03-10 17:02:14 -0300

    Acertando teamviewer

--- scripts/18-software/categories/internet/teamviewer.sh
+++ scripts/18-software/categories/internet/teamviewer.sh
@@ -2,17 +2,10 @@
 
 echo "       - Team Viewer..."
 
-FILES=$(dirname $0)/files
-APT=/etc/apt/sources.list.d/teamviewer.list
-KEY=/usr/share/keyrings/teamviewer-keyring.gpg
-
 LOCK=/tmp/workstation.lock.internet.teamviewer
-[ -f ${LOCK} ] && return
+# [ -f ${LOCK} ] && return
 
-# cp -f ${FILES}/teamviewer-keyring.gpg /usr/share/keyrings
-# echo "deb [signed-by=${KEY}] https://linux.teamviewer.com/deb stable main" > ${APT}
-
-# apt-get update                
-# apt-get install -y teamviewer 
+FILES=$(dirname $0)/files
+dpkg -i ${FILES}/teamviewer_15.39.3_amd64.deb > /dev/null 2>&1
 
 > ${LOCK}
