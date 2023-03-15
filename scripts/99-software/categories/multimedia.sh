ee1e5b418b7334db6c8f333cfc9bac6ccbf411d2
Leonardo Thibes <leonardothibes@gmail.com>
2023-03-15 20:36:47 -0300

    wip

--- scripts/99-software/categories/multimedia.sh
+++ scripts/99-software/categories/multimedia.sh
@@ -5,12 +5,14 @@ echo "   - Multimedia..."
 LOCK=/tmp/workstation.lock.multimedia
 [ -f ${LOCK} ] && return
 
-apt-get --fix-broken install > /dev/null 2>&1
-apt-get install -y audacity       \
-                   kazam          \
-                   handbrake      \
-                   kdenlive       \
-                   spotify-client \
-> /dev/null 2>&1
+# apt-get --fix-broken install > /dev/null 2>&1
+# apt-get install -y audacity       \
+#                    kazam          \
+#                    handbrake      \
+#                    kdenlive       \
+#                    spotify-client \
+# > /dev/null 2>&1
+
+apt-get install -y spotify-client > /dev/null 2>&1
 
 > ${LOCK}
