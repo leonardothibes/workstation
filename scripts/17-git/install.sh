55dd52d14d02ec85d74a5175ecd9c36d9fc99ca5
Leonardo Thibes <leonardothibes@gmail.com>
2023-03-15 16:42:15 -0300

    locks

--- scripts/17-git/install.sh
+++ scripts/17-git/install.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-echo " - GIT..."
+echo " - Git..."
 CONFS=$(dirname $0)/confs
 
+LOCK=/tmp/workstation.lock.git
+[ -f ${LOCK} ] && return
+
 apt-get install -y git git-flow > /dev/null 2>&1
+
+> ${LOCK}
