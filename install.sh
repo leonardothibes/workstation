4e80d99b6beb7f01a1be5a84e70c38a47a254190
Leonardo Thibes <leonardothibes@gmail.com>
2023-03-15 16:49:13 -0300

    Restaurando funcionamento original

--- install.sh
+++ install.sh
@@ -17,28 +17,6 @@ if [ ${WHO} -ne 0 ]; then
 fi
 # Only root
 
-sh ./scripts/00-backup/install.sh
-sh ./scripts/01-bash/install.sh
-sh ./scripts/02-skel/install.sh
-sh ./scripts/03-updates/install.sh
-sh ./scripts/04-utils/install.sh
-sh ./scripts/05-sysctl/install.sh
-sh ./scripts/06-vim/install.sh
-sh ./scripts/07-ruby/install.sh
-sh ./scripts/08-python/install.sh
-sh ./scripts/09-php/install.sh
-sh ./scripts/10-java/install.sh
-sh ./scripts/11-node/install.sh
-sh ./scripts/12-dotnet/install.sh
-sh ./scripts/13-ngrok/install.sh
-sh ./scripts/14-docker/install.sh
-sh ./scripts/15-snap/install.sh
-sh ./scripts/16-multipass/install.sh
-sh ./scripts/17-git/install.sh
-sh ./scripts/18-svn/install.sh
-sh ./scripts/19-zsh/install.sh
-exit 0
-
 # The magic happens here
 for DIR in $(ls ${HERE}/scripts)
 do
