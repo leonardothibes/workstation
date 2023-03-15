1115cef94570181da0a7fc96724d8a91813a8a24
Leonardo Thibes <leonardothibes@gmail.com>
2023-03-15 17:49:23 -0300

    wip

--- scripts/99-software/categories/development.sh
+++ scripts/99-software/categories/development.sh
@@ -17,15 +17,13 @@ apt-get install -y sqlitebrowser   \
 flatpak install -y --noninteractive flathub rest.insomnia.Insomnia > /dev/null 2>&1
 flatpak install -y --noninteractive flathub com.getpostman.Postman > /dev/null 2>&1
 flatpak install -y --noninteractive flathub io.beekeeperstudio.Studio > /dev/null 2>&1
+flatpak install -y --noninteractive flathub io.github.shiftey.Desktop > /dev/null 2>&1
 # From Flathub
 
 # Other sources
 echo "     - 3rd party..."
 
 DEVELOPMENT=$(dirname $0)/development
-sh ${DEVELOPMENT}/azurestudio.sh
-sh ${DEVELOPMENT}/mysql-workbench.sh
-
 for SOFTWARE in $(ls ${DEVELOPMENT})
 do
     chmod 755 ${DEVELOPMENT}/${SOFTWARE}
