9a89b6286e59c3cb656c333ffc8136e4b6564d82
Leonardo Thibes <leonardothibes@gmail.com>
2023-03-15 21:12:58 -0300

    Office

---
+++ scripts/99-software/categories/office.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "   - Office..."
+
+LOCK=/tmp/workstation.lock.office
+[ -f ${LOCK} ] && return
+
+# From Flathub
+flatpak install -y --noninteractive flathub com.wps.Office                   > /dev/null 2>&1
+flatpak install -y --noninteractive flathub com.ticktick.TickTick            > /dev/null 2>&1
+flatpak install -y --noninteractive flathub com.notesnook.Notesnook          > /dev/null 2>&1
+flatpak install -y --noninteractive flathub net.codeindustry.MasterPDFEditor > /dev/null 2>&1
+flatpak install -y --noninteractive flathub com.github.marktext.marktext     > /dev/null 2>&1
+# From Flathub
+
+> ${LOCK}
