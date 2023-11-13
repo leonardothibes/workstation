2221754c44591bd52f11d71d2d1f4fd1fc3c5235
Leonardo Thibes <leonardothibes@gmail.com>
2023-11-13 09:53:02 -0300

    multipass-sshfs

--- scripts/16-multipass/install.sh
+++ scripts/16-multipass/install.sh
@@ -6,6 +6,6 @@ CONFS=$(dirname $0)/confs
 LOCK=/tmp/workstation.lock.multipass
 [ -f ${LOCK} ] && return
 
-snap install multipass > /dev/null 2>&1
+snap install multipass multipass-sshfs > /dev/null 2>&1
 
 > ${LOCK}
