9541900e4970b3d4fd92bb68fe47d5778eb89d86
Leonardo Thibes <leonardothibes@gmail.com>
2024-02-22 00:49:53 -0300

    Mais um tentativa de tentar baixar o composer

--- core/scripts/09-php/install.sh
+++ core/scripts/09-php/install.sh
@@ -5,38 +5,39 @@ echo " - PHP..."
 export DEBIAN_FRONTEND=noninteractive
 CONFS=$(dirname $0)/confs
 BINS=$(dirname $0)/bin
-VERSION=8.1
 
 LOCK=/tmp/workstation.lock.php
 [ -f ${LOCK} ] && return
 
 # PHP
+PHP_VERSION=8.1
 apt-get install -y software-properties-common > /dev/null 2>&1
-apt-get install -y php${VERSION}          \
-                   php${VERSION}-curl     \
-                   php${VERSION}-gd       \
-                   php${VERSION}-ldap     \
-                   php${VERSION}-mbstring \
-                   php${VERSION}-mysql    \
-                   php${VERSION}-pgsql    \
-                   php${VERSION}-sybase   \
-                   php${VERSION}-sqlite3  \
-                   php${VERSION}-intl     \
-                   php${VERSION}-xml      \
-                   php${VERSION}-dev      \
-                   php${VERSION}-xdebug   \
+apt-get install -y php${PHP_VERSION}          \
+                   php${PHP_VERSION}-curl     \
+                   php${PHP_VERSION}-gd       \
+                   php${PHP_VERSION}-ldap     \
+                   php${PHP_VERSION}-mbstring \
+                   php${PHP_VERSION}-mysql    \
+                   php${PHP_VERSION}-pgsql    \
+                   php${PHP_VERSION}-sybase   \
+                   php${PHP_VERSION}-sqlite3  \
+                   php${PHP_VERSION}-intl     \
+                   php${PHP_VERSION}-xml      \
+                   php${PHP_VERSION}-dev      \
+                   php${PHP_VERSION}-xdebug   \
 > /dev/null 2>&1
 
-cp -f ${CONFS}/php.ini /etc/php/${VERSION}/cli
-cp -f ${CONFS}/xdebug.ini /etc/php/${VERSION}/mods-available
+cp -f ${CONFS}/php.ini /etc/php/${PHP_VERSION}/cli
+cp -f ${CONFS}/xdebug.ini /etc/php/${PHP_VERSION}/mods-available
 cp -f ${CONFS}/freetds.conf /etc/freetds/freetds.conf
 
-ln -sf /etc/php/${VERSION}/cli/php.ini /etc/php.ini
-ln -sf /usr/bin/php${VERSION} /etc/alternatives/php
+ln -sf /etc/php/${PHP_VERSION}/cli/php.ini /etc/php.ini
+ln -sf /usr/bin/php${PHP_VERSION} /etc/alternatives/php
 # PHP
 
 # COMPOSER
-cp -f ${BINS}/composer /usr/local/bin/composer
+COMPOSER_VERSION=2.7.1
+curl -s https://getcomposer.org/download/${COMPOSER_VERSION}/composer.phar -o /usr/local/bin/composer
 chmod 755 /usr/local/bin/composer
 # COMPOSER
 
