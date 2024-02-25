7d164c1da27a96f2e44fe5a311b300b237f8bb2a
Leonardo Thibes <leonardothibes@gmail.com>
2024-02-25 18:59:55 -0300

    tips

--- install.sh
+++ install.sh
@@ -5,3 +5,14 @@ echo " My Linux Workstation Config"
 HERE=$(dirname $0)
 
 sh "${HERE}/core/install.sh"
+
+echo " GIT"
+echo ""
+echo "  git config --global user.name \"Leonardo Thibes\""
+echo "  git config --global user.email \"leonardothibes@gmail.com\""
+echo ""
+
+echo " NGROK"
+echo ""
+echo "  ngrok config add-authtoken your-token-here"
+echo ""
