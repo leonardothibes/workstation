6765bbb787161d4ff88cd8f305e84a99e82136c1
Leonardo Thibes <leonardothibes@gmail.com>
2024-02-25 23:30:13 -0300

    postbird

--- tools-development/scripts/03-sqltools/install.sh
+++ tools-development/scripts/03-sqltools/install.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 echo " - SQL Tools..."
+FILES=$(dirname $0)/files
 
 LOCK=/tmp/workstation.lock.sqltools
 [ -f ${LOCK} ] && return
 
-apt-get install -y sqlitebrowser > /dev/null 2>&1
+apt-get install -y sqlitebrowser                                      > /dev/null 2>&1
 flatpak install -y --noninteractive flathub io.beekeeperstudio.Studio > /dev/null 2>&1
 
 # Postbird
@@ -13,8 +14,11 @@ URL="https://github.com/Paxa/postbird/releases/download/0.8.4/Postbird_0.8.4_amd
 FILE=/tmp/postbird.deb
 
 wget -q $URL -O $FILE
-dpkg -i $FILE
+dpkg -i $FILE > /dev/null 2>&1
 rm -f $FILE
+
+[ -f /usr/share/pixmaps ]      || cp -f $FILES/postbird.png /usr/share/pixmaps
+[ -f /usr/share/applications ] || cp -f $FILES/postbird.png /usr/share/applications
 # Postbird
 
 > ${LOCK}
