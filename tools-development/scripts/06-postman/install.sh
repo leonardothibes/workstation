ba3fb443cfee818806f57bf91fd877b493a8c8ed
Leonardo Thibes <leonardothibes@gmail.com>
2024-02-25 22:22:01 -0300

    Insomnia

--- tools-development/scripts/06-postman/install.sh
+++ tools-development/scripts/06-postman/install.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-echo " - Postman..."
+echo " - Postman and Insomnia..."
 
-LOCK=/tmp/workstation.lock.postman
+LOCK=/tmp/workstation.lock.postman_and_insomnia
 [ -f ${LOCK} ] && return
 
 flatpak install -y --noninteractive flathub com.getpostman.Postman > /dev/null 2>&1
+flatpak install -y --noninteractive flathub rest.insomnia.Insomnia > /dev/null 2>&1
 
 > ${LOCK}
