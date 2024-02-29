93ac61372ff1eee5c4742e32e2dfb441c2e92c9c
Leonardo Thibes <leonardothibes@gmail.com>
2024-02-29 19:38:18 -0300

    inor

--- tools-development/scripts/06-http-tools/install.sh
+++ tools-development/scripts/06-http-tools/install.sh
@@ -2,7 +2,7 @@
 
 echo " - HTTP Tools..."
 
-LOCK=/tmp/workstation.lock.postman_and_insomnia
+LOCK=/tmp/workstation.lock.http_tools
 [ -f ${LOCK} ] && return
 
 flatpak install -y --noninteractive flathub com.getpostman.Postman > /dev/null 2>&1
