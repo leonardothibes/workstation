ee6755e969acf06056138f40f5b0e219f30b1e09
Leonardo Thibes <leonardothibes@gmail.com>
2024-02-29 19:59:33 -0300

    MINOR

--- tools-development/scripts/07-virtualization-tools/install.sh
+++ tools-development/scripts/07-virtualization-tools/install.sh
@@ -5,7 +5,8 @@ echo " - Virtualization Tools..."
 LOCK=/tmp/workstation.lock.virtualization_tools
 [ -f ${LOCK} ] && return
 
-VERSION=7.0
+# VIRTUALBOX
+VBOX_VERSION=7.0
 curl -sO https://www.virtualbox.org/download/oracle_vbox_2016.asc
 
 GPG=/usr/share/keyrings/oracle-virtualbox-2016.gpg
@@ -15,8 +16,10 @@ gpg -o ${GPG} --dearmor oracle_vbox_2016.asc
 REPO="deb [arch=amd64 signed-by=/usr/share/keyrings/oracle-virtualbox-2016.gpg] https://download.virtualbox.org/virtualbox/debian jammy contrib"
 echo ${REPO} > /etc/apt/sources.list.d/virtualbox.list
 
-apt-get update                           > /dev/null 2>&1
-apt-get install -y virtualbox-${VERSION} > /dev/null 2>&1
+apt-get update                                > /dev/null 2>&1
+apt-get install -y virtualbox-${VBOX_VERSION} > /dev/null 2>&1
 
 rm -f oracle_vbox_2016.asc
+# VIRTUALBOX
+
 > ${LOCK}
