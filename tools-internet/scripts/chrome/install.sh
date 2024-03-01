f6d730aa4fd09b437d4ae841a272fefd83a1eb49
Leonardo Thibes <leonardothibes@gmail.com>
2024-02-29 21:55:19 -0300

    fix

--- tools-internet/scripts/chrome/install.sh
+++ tools-internet/scripts/chrome/install.sh
@@ -5,8 +5,8 @@ echo " - Google Chrome..."
 LOCK=/tmp/workstation.lock.internet.chrome
 [ -f ${LOCK} ] && return
 
-APT="/etc/apt/sources.list.d/google.list"
-echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" > ${APT}
+# APT="/etc/apt/sources.list.d/google.list"
+# echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" > ${APT}
 wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | apt-key add - > /dev/null 2>&1
 
 apt-get update                          > /dev/null 2>&1
