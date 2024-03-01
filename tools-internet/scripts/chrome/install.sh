5f7fdaaac0fa907d5a89d658a2c7427829aebb95
Leonardo Thibes <leonardothibes@gmail.com>
2024-02-29 22:15:39 -0300

    fix: chrome

--- tools-internet/scripts/chrome/install.sh
+++ tools-internet/scripts/chrome/install.sh
@@ -5,11 +5,14 @@ echo " - Google Chrome..."
 LOCK=/tmp/workstation.lock.internet.chrome
 [ -f ${LOCK} ] && return
 
-# APT="/etc/apt/sources.list.d/google.list"
-# echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" > ${APT}
-wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | apt-key add - > /dev/null 2>&1
+KEY=/tmp/google.pub
+
+wget -q https://dl-ssl.google.com/linux/linux_signing_key.pub -O $KEY
+gpg --no-default-keyring --keyring /etc/apt/keyrings/google-chrome.gpg --import $KEY > /dev/null 2>&1
+echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/google-chrome.gpg] http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list
 
 apt-get update                          > /dev/null 2>&1
 apt-get install -y google-chrome-stable > /dev/null 2>&1
 
+rm -f $KEY
 > ${LOCK}
