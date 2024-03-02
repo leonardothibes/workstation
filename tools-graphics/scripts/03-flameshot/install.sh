485678c4ce984bee082f1786ffb11ff8bb7cebc0
Leonardo Thibes <leonardothibes@gmail.com>
2024-03-02 18:23:53 -0300

    fix: flameshot pelp apt

--- tools-graphics/scripts/03-flameshot/install.sh
+++ tools-graphics/scripts/03-flameshot/install.sh
@@ -5,6 +5,6 @@ echo " - Flameshot..."
 LOCK=/tmp/workstation.lock.graphics.flameshot
 [ -f ${LOCK} ] && return
 
-flatpak install -y --noninteractive flathub org.flameshot.Flameshot > /dev/null 2>&1
+apt-get install -y flameshot > /dev/null 2>&1
 
 > ${LOCK}
